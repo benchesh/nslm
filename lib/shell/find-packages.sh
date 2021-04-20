9fc2d37029ba1e5dfb9daa555c2b544ee0572dc2
Ben Cheshire <20292346+benchesh@users.noreply.github.com>
2021-04-21 00:28:31 +0100

    better method to exclude nslm from register

--- lib/shell/find-packages.sh
+++ lib/shell/find-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find "$1" -name "package.json" -not -path "*/node_modules/*" -not -path "*/.yalc/*" -not -path "*/.nslm/*"
+find "$1" -name "package.json" -not -path "*/node_modules/*" -not -path "*/.yalc/*"
