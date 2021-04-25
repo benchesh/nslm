7cc3aa5714718d8d044729a3f93e8c95dc0445a1
Ben Cheshire <20292346+benchesh@users.noreply.github.com>
2021-04-25 22:14:37 +0100

    imports moved to run.js

--- bin/run.js
+++ bin/run.js
@@ -11,26 +11,28 @@ const moduleExists = (path) => {
     }
 }
 
+const { importAll } = require('../lib/importAll.js');
+importAll().from('./common.js');
+
 if (!args[0].includes('..') && moduleExists(cmd)) {//if the cmd exists, run it!
     const yargs = require('yargs/yargs');
-    require(cmd).run(
-        yargs(args.splice(1))
-            .array(['modules', 'pathscontainingevery', 'pathscontainingsome'])
-            .boolean(['all', 'allowmissing'])
-            .alias('pathcontainingsome', 'pathscontainingsome')
-            .alias('pathcontainingany', 'pathscontainingsome')
-            .alias('pathcontaining', 'pathscontainingsome')
-            .alias('pathscontaining', 'pathscontainingsome')
-            .alias('pathcontainingevery', 'pathscontainingevery')
-            .alias('pathcontainingall', 'pathscontainingevery')
-            .alias('pathscontainingall', 'pathscontainingevery')
-            .alias('module', 'modules')
-            .alias('a', 'all')
-            .argv
-    );
+
+    global.argv = yargs(args.splice(1))
+        .array(['modules', 'pathscontainingevery', 'pathscontainingsome'])
+        .boolean(['all', 'allowmissing', 'simulate'])
+        .alias('pathcontainingsome', 'pathscontainingsome')
+        .alias('pathcontainingany', 'pathscontainingsome')
+        .alias('pathcontaining', 'pathscontainingsome')
+        .alias('pathscontaining', 'pathscontainingsome')
+        .alias('pathcontainingevery', 'pathscontainingevery')
+        .alias('pathcontainingall', 'pathscontainingevery')
+        .alias('pathscontainingall', 'pathscontainingevery')
+        .alias('module', 'modules')
+        .alias('a', 'all')
+        .argv
+
+    require(cmd).run();
     return;
 }
 
-const { importAll } = require('../lib/importAll.js');
-importAll().from('./common.js');
 error(`${args[0]} is not a valid ${mname} command!`)
--- lib/shell/find-packages.sh
+++ lib/shell/find-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find "$1" -name "package.json" -not -path "*/node_modules/*" -not -path "*/.yalc/*"
+find "$1" -name "package.json" -not -path "*/.yalc/*"
